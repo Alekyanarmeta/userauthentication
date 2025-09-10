b074be735867266f31e772dc1c570cba7b4fe7e0
NarmetaAlekya <2203a51835@sru.edu.in>
2025-09-10 12:44:57 +0530

    intial commit

---
+++ src/main.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import SimpleFrontendAuthApp from "./SimpleFrontendAuth.jsx";
+import "./index.css"; // Tailwind CSS import
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <SimpleFrontendAuthApp />
+  </React.StrictMode>
+);
